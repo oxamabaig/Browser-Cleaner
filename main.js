864d216c506ee4e126ece506c21b626bd841710f
oxamabaig <uxamabaig235@gmail.com>
2020-03-26 00:45:14 +0500

    v_1.0_front-end

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  mainWindow.setMenu(null)
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
